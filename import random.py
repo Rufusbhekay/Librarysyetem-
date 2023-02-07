1a41c0b7c13c3e21be460b6315313f623def34ea
Rufusbrad <oyegbileoluwabukunmi20@gmail.com>
2023-02-07 13:24:44 +0000

    Second commit

--- import random.py
+++ import random.py
@@ -16,8 +16,8 @@ while True:
     random_number = random.randint(0, 2)
     # rock: 0, paper: 1, scissors: 2
     computer_pick = options[random_number]
-    print ("Computer picked", computer_pick + ".")
-    print ("You picked", user_input, "." )
+    print ("Computer picked: ", computer_pick + ".")
+    print ("You picked: ", user_input, "." )
     
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!") 
@@ -32,12 +32,11 @@ while True:
     elif user_input == "scissors" and computer_pick == "paper":
         print("You won!") 
         user_wins += 1
-    
+
     elif user_input == "scissor" and computer_pick == "rock":
         print("Computer won!") 
         computer_wins += 1
         
-
     elif user_input == "rock" and computer_pick == "paper":
         print("Computer won!") 
         computer_wins += 1
